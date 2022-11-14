22469e2e5f8922120f65ea3eec14ce3b681c27ea
Giulio Iannello <g.iannello@unicampus.it>
2022-11-14 15:35:44 +0100

    Implementata una classe di test sulle mosse della Torre. Aggiunta la verifica di mossa della torre lungo una riga nel caso vi siano pezzi nelle caselle intermedie. Modificati i cicli che verificano se le celle sono occupate per gestire correttamente i casi in cui ci si muove per righe o colonne decrescenti.

--- Torre.py
+++ Torre.py
@@ -35,12 +35,24 @@ class Torre(Pezzo):
         """
         if super().verifica_mossa(destinazione):  # le condizioni generiche sono verificate
             if self.posizione[0] == destinazione[0]:  # la mossa è lungo la stessa colonna
-                for riga in range(self.posizione[1]+1, destinazione[1]):
-                    if not self.scacchiera.get_pezzo([destinazione[0], riga]) == None:
+                # verifica che non ci siano pezzi tra la casella di partenza e quella di arrivo
+                # per gestire corretamente il ciclo deve distinguere il caso di mossa per righe crescenti o decrescenti
+                first = self.posizione[1]+1 if self.posizione[1]+1 < destinazione[1] else destinazione[1]+1  # prima riga da esaminare
+                last = destinazione[1] if self.posizione[1]+1 < destinazione[1] else self.posizione[1]  # ultma riga da esaminare
+                for riga in range(first, last):
+                    if not self.scacchiera.get_pezzo([destinazione[0], riga]) == None:  # la casella è occupata
                         print(f"La mossa non è legale perché è presente un pezzo ({self.scacchiera.get_pezzo([destinazione[0], riga]).nome}) nella casella {destinazione[0]}{riga}")
                         return False
                 return True
             elif self.posizione[1] == destinazione[1]:  # la mossa è lungo la stessa riga
+                # verifica che non ci siano pezzi tra la casella di partenza e quella di arrivo
+                # per gestire corretamente il ciclo deve distinguere il caso di mossa per colonne crescenti o decrescenti
+                first = ord(self.posizione[0])+1 if ord(self.posizione[0])+1 < ord(destinazione[0]) else ord(destinazione[0])+1  # prima colonna da esaminare
+                last = ord(destinazione[0]) if ord(self.posizione[0])+1 < ord(destinazione[0]) else ord(self.posizione[0])  # ultma colonna da esaminare
+                for col in range(first, last):
+                    if not self.scacchiera.get_pezzo([chr(col), destinazione[1]]) == None:  # la casella è occupata
+                        print(f"La mossa non è legale perché è presente un pezzo ({self.scacchiera.get_pezzo([chr(col), destinazione[1]]).nome}) nella casella {chr(col)}{destinazione[1]}")
+                        return False
                 return True
             else:
                 print(f'La mossa {self.posizione[0]}{self.posizione[1]}, {destinazione[0]}{destinazione[1]} non è legale per la Torre')
